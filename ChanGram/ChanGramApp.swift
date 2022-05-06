60f326a85501b1e52807c646f272344bf4589d65
Weixian <wx@weareday.one>
2022-05-06 09:41:12 +0800

    Initial Commit

---
+++ ChanGram/ChanGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChanGramApp.swift
+//  ChanGram
+//
+//  Created by Chan Wai Hsuen on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ChanGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
