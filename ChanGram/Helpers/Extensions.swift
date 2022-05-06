cd27bb3eaf678b0e48d9e7cc77c7042ea436b382
Weixian <wx@weareday.one>
2022-05-06 18:02:09 +0800

    chore: extension

---
+++ ChanGram/Helpers/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  ChanGram
+//
+//  Created by Chan Wai Hsuen on 6/5/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    struct MyTheme {
+        
+        static var purpleColor : Color {
+            return Color("ColorPurple")
+        }
+        
+        static var yellowColor : Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor : Color {
+            return Color("ColorBeige")
+        }
+    }
+}
